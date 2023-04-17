e25952a56d2f6b1af0c13595452100388f3438b8
tonyJordao <tonyjordao2019@gmail.com>
2023-04-16 22:04:16 -0300

    atualizacao do domingo a noite

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom"
 function App () {
 return( 
   <BrowserRouter>
+  
     <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/projetos" element={<Projetos />}/>
     <Route path="/contato" element={<Contato />}/>
       
+
     </Routes>
   </BrowserRouter>
   
